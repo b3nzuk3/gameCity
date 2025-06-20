de1001118b17496f0152c82109e9ee36b5f5a693
Hussein Salim <hussenito7@gmail.com>
2025-06-20 22:18:47 +0300

    api issue

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -10,7 +10,7 @@ import axios from 'axios'
 import backendService from './backendService'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://gamecity-backend-i2sy.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
