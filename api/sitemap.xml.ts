64b23d4a77e271131c0299c61addc7b9c4a54b44
Hussein Salim <hussenito7@gmail.com>
2025-10-27 23:39:04 +0300

    Add 301 redirects from .com to .co.ke domain

--- api/sitemap.xml.ts
+++ api/sitemap.xml.ts
@@ -187,7 +187,7 @@ export async function GET(request: Request) {
     const fallbackXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://www.gamecityelectronics.com/</loc>
+    <loc>https://www.gamecityelectronics.co.ke/</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
